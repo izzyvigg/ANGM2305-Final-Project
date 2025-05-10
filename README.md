e3a1171fd998028a53ef23011c5c57641d2c5804
izzyvigg <izzyvigg@gmail.com>
2025-05-10 17:12:27 -0500

    define main function and prompt the user to select a file from their computer + added README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 2305finalProject
+
+## Demo
+Demo Video: <URL>
+
+## GitHub Repository
+GitHub Repo: <URL>
+
+## Description
+
+
+## References
+Building a color palette generator in Python: <https://kylermintah.medium.com/coding-a-color-palette-generator-in-python-inspired-by-procreate-5x-b10df37834ae>
+Color Picker - Python Tkinter GUI Tutorial #49: <https://www.youtube.com/watch?v=NDCirUTTrhg>
--- src/project.py
+++ src/project.py
@@ -1,2 +1,32 @@
+import math
+import PIL
+import extcolors
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+from matplotlib import gridspec
+from tkinter import Tk, filedialog
+
 def main():
-    
+    # Get the image file from the user and run the window
+    image_path = get_image_file()
+    if image_path:
+        study_image(image_path)
+    else:
+        print("No image file selected.")
+
+def fetch_image(image_path):
+    return PIL.Image.open(image_path)
+
+def study_image(image_path):
+    img = fetch_image(image_path)
+
+def get_image_file():
+    root = Tk()
+    root.withdraw()
+    return filedialog.askopenfilename(
+        title="Select an image file",
+        filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif")]
+    )
+
+if __name__ == "__main__":
+    main()
